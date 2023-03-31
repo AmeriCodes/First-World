4968fe4614e6559c31edd5103f84c44402c2913f
Mateus Américo <mateus-americo@live.com>
2023-03-31 16:40:22 -0300

    Update Ex008.py

--- Ex008.py
+++ Ex008.py
@@ -1,11 +1,13 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e minímetros
+print('==' * 15)
+m = float(input('Digite uma medida em metros: '))
+print('==' * 15)
 
-print('==' *15)
-metro = int(input('Digite uma valor em metros: '))
-print('==' *15)
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
 
-cen = metro * 100
-
-mil = metro * 1000
-
-print(f'Convertendo para centímetros resulta em {cen}.\nE convertendo para milímetros temos {mil}.')
+print(f'A medida de {m}metros corresponde a: \n{km} quilômetros \n{hm} hectômetros \n{dam} decâmetros \n{dm:.0f} decímetros \n{cm:.0f} centímetros \n{mm:.0f} milímetros')
