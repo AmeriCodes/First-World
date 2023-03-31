ad3af9d784265e98ddafe47b0aa9bd7afaecc304
Mateus Américo <mateus-americo@live.com>
2023-03-31 15:33:56 -0300

    Update Ex001.py

--- Ex001.py
+++ Ex001.py
@@ -1,4 +1,3 @@
-# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+# Crie um programa que mostre na tela a frase "Olá mundo"
 
-nome = input('Qual é o seu nome: ')
-print(f'Seja muito bem vindo {nome}!, é um prazer ter você conosco!')
+print('Olá mundo!')
