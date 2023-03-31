bf91609704df7d2826a6dce90154fe6d3eb49964
Mateus Américo <mateus-americo@live.com>
2023-03-31 16:06:48 -0300

    Create Ex006.py

---
+++ Ex006.py
@@ -0,0 +1,8 @@
+# Crie um algorítimo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
+
+print('=' *25)
+n = int(input('Digite um número inteiro: '))
+print('=' *25)
+
+print('Analisando o número digitado percebo que:')
+print(f'O dobro de {n} é {n * 2}.\nO triplo de {n} é {n * 3}.\nE a raiz quadrade de {n} é {n ** 2}.')
