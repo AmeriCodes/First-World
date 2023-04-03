fe43533c78fdf73f029afe8eacbff8d3af81e97f
Mateus Américo <mateus-americo@live.com>
2023-04-03 13:34:25 -0300

    Update Ex006.py

--- Ex006.py
+++ Ex006.py
@@ -4,5 +4,9 @@ print('=' *25)
 n = int(input('Digite um número inteiro: '))
 print('=' *25)
 
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
 print('Analisando o número digitado percebo que:')
-print(f'O dobro de {n} é {n * 2}.\nO triplo de {n} é {n * 3}.\nE a raiz quadrade de {n} é {n ** 2}.')
+print(f'O dobro de {n} é {d}.\nO triplo de {n} é {t}.\nE a raiz quadrada de {n} é {r:.2f}.')
