54d0d155369c0d7dac8ceb7219d12b9d35e93efa
Mateus Américo <mateus-americo@live.com>
2023-04-05 14:03:16 -0300

    Update Ex020.py

--- Ex020.py
+++ Ex020.py
@@ -2,6 +2,19 @@
 
 import random
 
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno: '))
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+random.shuffle(lista)
+print('A ordem de apresentação será: ')
+print(lista)
+
+"""import random
+
 aluno1 = input('Digite o nome do primeiro aluno: ')
 aluno2 = input('Digite o nome do segundo aluno: ')
 aluno3 = input('Digite o nome do terceiro aluno: ')
@@ -13,6 +26,4 @@ random.shuffle(alunos)
 
 print('A ordem de apresentação dos trablhos será: ')
 for i, aluno in enumerate(alunos):
-    print(f'{i+1}º - {aluno}')
-    
-    
+    print(f'{i+1}º - {aluno}')"""
