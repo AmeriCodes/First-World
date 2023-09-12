22266baf2fb04ff41220a28f4a9ae84050672d10
AmeriCodes <mateus-americo@live.com>
2023-09-12 16:59:25 -0300

    refactor: predecessor and successor, add validation

--- Exercícios/Ex005.py
+++ Exercícios/Ex005.py
@@ -1,2 +1,23 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 
+
+
+while True:
+    entry = str(input('Type a number: ')).strip()
+
+    try:
+        entry = int(entry)
+        break
+    except:
+        try:
+            entry = float(entry)
+            break
+        except:
+            print('Invalid entry, enter a number.')
+            continue
+
+successor = entry + 1
+predecessor = entry - 1
+
+print(f'The predecessor of {entry} is {predecessor}.')
+print(f'The successor of {entry} is {successor}.')
