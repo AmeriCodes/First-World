8450c56ad1a1bf0b8b88810427bdddf28ae72578
macc <mateus-americo@live.com>
2023-12-06 15:24:13 -0300

    refactor: double, triple and square root

--- Exercícios/Ex006.py
+++ Exercícios/Ex006.py
@@ -1,2 +1,7 @@
 # Crie um algorítimo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
 
+number = int(input('Type a number: '))
+
+print(f'The double {number} is {2 * number}.')
+print(f'The triple {number} is {3 * number}.')
+print(f'The square root of {number} is {number ** (1/2):.2f}.')
