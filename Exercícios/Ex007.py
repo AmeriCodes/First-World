a44e4720a06dcbbbd925f339a1abdd31e8e2f006
macc <mateus-americo@live.com>
2023-12-06 20:26:47 -0300

    refactor: student's average

--- Exercícios/Ex007.py
+++ Exercícios/Ex007.py
@@ -1,2 +1,10 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
 
+print(' ----- Averaging Calculation -----')
+
+grade1 = float(input('Grade 1: '))
+grade2 = float(input('Grade 2: '))
+
+avg = (grade1 + grade2) / 2
+
+print(f"The student average is {avg}.")
