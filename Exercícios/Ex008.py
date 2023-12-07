f5a50d29c6232dd80baee800fe58a1b840969df4
macc <mateus-americo@live.com>
2023-12-07 04:53:22 -0300

    refactor: Measurement converter

--- Exercícios/Ex008.py
+++ Exercícios/Ex008.py
@@ -1,2 +1,23 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e minímetros
+print('----- Measurement Converter -----')
+
+while True:
+    meters = str(input('\nMeters: ')).strip()
+    try:
+        meters = float(meters)
+        break
+    except ValueError:
+        print('Invalid input!\nPlease try again.')
+        continue
+
+kilometers = meters / 1000
+hectometers = meters / 100
+decameters = meters / 10
+decimeters = meters * 10
+centimeters = meters * 100
+millimeters = meters * 1000
+
+print('\n----- Successful Conversion -----\n')
+
+print(f'{kilometers=}\n{hectometers=}\n{decameters=}\n{meters=}\n{decimeters=}\n{centimeters=}\n{millimeters=}')
 
