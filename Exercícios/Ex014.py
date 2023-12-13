0a207b26f1550c5ace1afe8a55f5bed0b3fd5b58
macc <mateus-americo@live.com>
2023-12-13 14:14:12 -0300

    refactor: Cº to F

--- Exercícios/Ex014.py
+++ Exercícios/Ex014.py
@@ -1,2 +1,10 @@
 # Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
 
+print(f'{" Temperature Conversion ":-^47}')
+
+celsius = str(input('Enter the temperature in degrees Celsius (°C): '))
+celsius = float(celsius)
+
+fahrenheit = celsius * 1.8 + 32
+
+print(f'\nThe temperature of {celsius}°C is equivalent to {fahrenheit}°F')
