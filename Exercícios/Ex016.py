a9fdd1c733415be865f40671b2ad7ceb80fbb8ff
macc <mateus-americo@live.com>
2023-12-20 17:39:25 -0300

    refactor: getting the integer part of a number

--- Exercícios/Ex016.py
+++ Exercícios/Ex016.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. Ex: digite o número: 6.127, O número 6.127 tem a parte inteira de 6.
 
+entry = float(input('Enter a real number to find its integer part: '))
+
+print(f'The integer part of {entry} is {int(entry)}')
