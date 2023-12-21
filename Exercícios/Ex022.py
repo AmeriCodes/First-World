d28d153d92734625f77c4ddd3c8e89752f6e7784
macc <mateus-americo@live.com>
2023-12-21 16:00:54 -0300

    refactor: analyzing names

--- Exercícios/Ex022.py
+++ Exercícios/Ex022.py
@@ -4,3 +4,16 @@
 # 3. Quantas letras ao todo (sem considerar espaços)
 # 4. Quantas letras tem o primeiro nome.
 
+full_name = str(input('Enter your full name: ')).strip()
+
+print(full_name.upper())
+print(full_name.lower())
+
+splited_name = full_name.split()
+concatenated_name = ''.join(splited_name)
+
+print(f'At all, we have {len(concatenated_name)} letters in {full_name}.')
+
+# print(f'At all, we have {len(full_name) - full_name.count(" ")} letters in {full_name}.')
+
+print(f'Your first name {full_name[:full_name.find(" ")]} has {full_name.find(" ")} letters.')
