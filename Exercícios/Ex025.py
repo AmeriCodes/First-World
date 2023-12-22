7fc7c7e31e074e9985281139954f4ac2efb0e665
macc <mateus-americo@live.com>
2023-12-22 18:35:40 -0300

    refactor: does your name contain Silva?

--- Exercícios/Ex025.py
+++ Exercícios/Ex025.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome.
 
+name = str(input("What's your full name? ")).strip()
+
+print(f'Does your name contain Silva? {"silva" in name.lower()}')
