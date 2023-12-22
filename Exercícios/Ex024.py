57dc84854eb8aef4059ae1d99c29e8e58589bfce
macc <mateus-americo@live.com>
2023-12-22 18:34:38 -0300

    refactor: city Santos

--- Exercícios/Ex024.py
+++ Exercícios/Ex024.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa o não com o nome "SANTO".
 
+city = str(input("Enter your home city: ")).strip()
+
+print(city[:5].title() == 'Santo')
