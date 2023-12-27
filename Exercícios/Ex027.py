1a6acbd422bcb1a69103df12a2d32ebff8952a8d
macc <mateus-americo@live.com>
2023-12-27 20:34:43 -0300

    refactor: full name

--- Exercícios/Ex027.py
+++ Exercícios/Ex027.py
@@ -1,2 +1,8 @@
 # Faça um programa que leia o nome completo de uma pessoa mostrando em seguida o primeiro e o último nome separadamente.
 
+name = str(input("Enter your full name: ")).strip()
+
+name = name.split()
+print('Nice to meet you!')
+print(f'Your first name is {name[0]}.')
+print(f'Your last name is {name[-1]}.')
