2a2102cc781c30bdf6901404514fcd90435195da
macc <mateus-americo@live.com>
2023-12-28 11:51:38 -0300

    refactor: Try to guess the number I'm thinking...

--- Exercícios/Ex028.py
+++ Exercícios/Ex028.py
@@ -1,3 +1,13 @@
 # Escreva um programa que faça o computador 'pensar' em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+from random import randint
+
+print("I'll think of a number between 0 and 5. Try to guess!")
+pc_number = randint(0, 5)
+entry = int(input('Enter a number between 0 and 5: '))
+
+if pc_number == entry:
+    print("Congratulations, you win!")
+else:
+    print(f"Oh, you lose!\nI was thinking of {pc_number}.")
