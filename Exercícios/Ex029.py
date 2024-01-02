a156ca4446168e2b04ef4e3cba07abbebda62a33
macc <mateus-americo@live.com>
2024-01-02 14:23:18 -0300

    refactor: calculate_penalty

--- Exercícios/Ex029.py
+++ Exercícios/Ex029.py
@@ -1,2 +1,14 @@
 # Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. Á multa vai custar R$7,00 por cada Km acima do limite.
 
+def calculate_penalty(speed):
+
+    cost_per_excess_km = 7.00
+    penalty_amount = (speed - 80) * cost_per_excess_km
+    return penalty_amount
+
+velocity = int(input("Enter the car's speed: "))
+
+if velocity <= 80:
+    print("Have a nice day, drive safely!")
+else:
+    print(f"You exceeded 80km, the speed limit!\nThe penalty will be {calculate_penalty(velocity):.2f}R$")
