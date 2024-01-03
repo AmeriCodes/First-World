b76ed6616f038d7db59890048930ce0708ca56e8
macc <mateus-americo@live.com>
2024-01-03 16:00:47 -0300

    refactor: even and odd number

--- Exercícios/Ex030.py
+++ Exercícios/Ex030.py
@@ -1,2 +1,9 @@
 # Crie um programa que leia um número inteiro e moestre na tela se ele é PAR ou IMPAR.
 
+number = int(input('Enter a integer number: '))
+
+if number % 2 == 0:
+    print(f'{number} is a EVEN number!')
+else:
+    print(f'{number} is a ODD number!')
+
