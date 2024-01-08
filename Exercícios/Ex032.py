f4eb288672eb0b31d7214331463c17712e588595
macc <mateus-americo@live.com>
2024-01-08 08:18:09 -0300

    refactor: leap year

--- Exercícios/Ex032.py
+++ Exercícios/Ex032.py
@@ -1,2 +1,16 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
- 
+
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+year = int(input('Is that a leap year? Enter to know: '))
+
+if is_leap_year(year):
+    print(f"The year {year} is a leap year.")
+else:
+    print(f"The year {year} is not a leap year.")
